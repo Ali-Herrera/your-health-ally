dcdc3ca27e2d0babc595586c450bceb0a96a51e8
Victoria <vmcnorrill@gmail.com>
2024-02-23 14:39:13 -0500

    cleaned up theme.colors

--- components/config/theme.ts
+++ components/config/theme.ts
@@ -1,5 +1,9 @@
 import { MantineThemeOverride } from "@mantine/core";
 
+// This file is used to override the default Mantine theme.
+
+// To view Mantine default theme.colors: https://mantine.dev/colors-generator/
+
 // To generate a color array:
 // 1. Go to: https://omatsuri.app/color-shades-generator
 // 2. Set saturation shift to 0% and drag darken/lighten % slider all the way to the right.
@@ -8,39 +12,53 @@ import { MantineThemeOverride } from "@mantine/core";
 
 export const theme: MantineThemeOverride = {
 	colors: {
-		pink: [
-			"#EF96C3", // 0 = Lightest
-			"#E265A5",
-			"#DB5197",
-			"#D4408B",
-			"#CC3180",
-			"#B83075", // 5 = Light
-			"#9E2B65", // 6 = Medium
-			"#92255C",
-			"#872657", // 8 = Dark
-			"#7D214F",
-			"#731C48",
+		darkPink: [
+			"#EECDDE", // 0 = Lightest
+			"#DF9DBE",
+			"#D272A3",
+			"#C84D8B",
+			"#B63777",
+			"#9D2E66",
+			"#872657", // 6 = darkPink
+			"#6F2048",
+			"#5C1C3C",
+			"#4C1732", // 9 = Darkest
 		],
-		green: [
-			"#28A99E", // 0 = Lightest
-			"#05998C",
-			"#048C7F",
-			"#037C6E",
-			"#016458", // 4 = Light
-			"#036C5F", // 5 = Medium
-			"#025043", // 6 = Dark
-			"#004A3E",
-			"#004539",
-			"#003F34",
+		pink: [
+			"#EAC8D9",
+			"#E0A7C4",
+			"#D789B1",
+			"#D16D9F",
+			"#CB5290",
+			"#C83A82",
+			"#B83075", // 6 = pink
+			"#A12D68",
+			"#8E2A5D",
+			"#7C2752",
+			"#6D2549",
+			"#602241",
+			"#541F3A",
+		],
+		teal: [
+            "#C8EAE4",
+            "#77D2C3",
+            "#35C3AC",
+            "#219B87",
+            "#137C6B",
+            "#096354",
+            "#025043", // 6 = teal (dark green-blue)
+            "#05332B",
+            "#05211C",
+            "#051512",
 		],
 		yellow: [
-			"#FFFAE4", // 0 = Lightest
+			"#FFFAE4",
 			"#FEF4CF",
-			"#FAE8A0", // 2 = From Color Palette Idea Doc
+			"#FAE8A0", // 2 = Color Palette Ideas doc
 			"#F7DB6E",
 			"#F5D043",
 			"#F4C929",
-			"#F3C519",
+			"#F3C519", // 6 = yellow
 			"#D8AE0B",
 			"#C09A00",
 			"#A68500",
@@ -48,22 +66,35 @@ export const theme: MantineThemeOverride = {
 		orange: [
 			"#FFF3E5",
 			"#FDE5D2",
-			"#F6C8A5", // 2 = From Color Palette Idea Doc
+			"#F6C8A5", // 2 = Color Palette Idea doc
 			"#EFAA74",
 			"#EB914C",
 			"#E88131",
-			"#E77722",
+			"#E77722", // 6 = orange
 			"#CD6616",
 			"#B7590F",
 			"#A04C04",
 		],
+        red: [
+            "#FBF5F6",
+            "#F0D8DA",
+            "#E6BCC0",
+            "#DEA1A8",
+            "#D78891",
+            "#D2717B",
+            "#CE5A67", // 6 = red (a soft red)
+            "#C54856",
+            "#B83D4B",
+            "#A53A46",
+          ],
 	},
 	black: "#1A1910",
 	white: "#F9F9F9",
 
-	primaryColor: "pink",
+	primaryColor: "darkPink",
 	primaryShade: 8,
 
+    // Added components for examples as we build the app
 	components: {
 		Avatar: {
 			defaultProps: {
@@ -88,18 +119,17 @@ export const theme: MantineThemeOverride = {
 					fontStretch: "100%",
 					":hover": {
 						backgroundColor:
-							params.variant === "filled" ? theme.colors.fuchsia[6] : undefined,
+							params.variant === "filled" ? theme.colors.pink[8] : undefined,
 						color:
-							params.variant === "filled" ? theme.colors.fuchsia[0] : undefined,
+							params.variant === "filled" ? theme.colors.pink[0] : undefined,
 					},
 					":active": {
 						backgroundColor:
-							params.variant === "filled" ? theme.colors.fuchsia[6] : undefined,
+							params.variant === "filled" ? theme.colors.pink[8] : undefined,
 						color:
-							params.variant === "filled" ? theme.colors.fuchsia[0] : undefined,
+							params.variant === "filled" ? theme.colors.pink[0] : undefined,
 					},
 					":disabled": {
-						backgroundColor: "#E4E6E7",
 						filter: "grayscale(100%)",
 					},
 				},
