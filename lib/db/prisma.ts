e7f42c67f5fc6b85c2ccda6b10d5fde823486cb9
Ali H <alisonherrera02@gmail.com>
2024-02-26 08:40:08 -0500

    configured mongoDB and prisma for backend

---
+++ lib/db/prisma.ts
@@ -0,0 +1,19 @@
+//creates and exports a prisma client instance as singleton
+
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type prismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: prismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
