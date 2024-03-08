e172b3fc9e7c02a7ad2aefc69a515fe04274b097
Ali H <alisonherrera02@gmail.com>
2024-03-08 13:41:15 -0500

    more style changes V1

--- app/Chat/index.tsx
+++ app/Chat/index.tsx
@@ -7,19 +7,16 @@ import {
   AppShell,
   Burger,
   Textarea,
+  Center,
+  Avatar,
 } from '@mantine/core';
-import { useMediaQuery, useDisclosure, useInViewport } from '@mantine/hooks';
+import { useDisclosure } from '@mantine/hooks';
 import { theme } from '../../components/config/theme';
-import {
-  UserButton,
-  useUser,
-  SignedIn,
-  SignedOut,
-  SignInButton,
-} from '@clerk/nextjs';
+import { UserButton, SignedIn } from '@clerk/nextjs';
 
 export function Chat() {
   const [opened, { toggle }] = useDisclosure();
+
   return (
     <div style={{ overflowX: 'hidden' }}>
       <div style={{ maxWidth: '100vw', position: 'relative' }}>
@@ -52,11 +49,35 @@ export function Chat() {
 
           <AppShell.Main
             bg={theme.colors.teal[1]}
-            style={{ overflowX: 'hidden' }}
+            style={{
+              overflowX: 'hidden',
+              paddingBottom: '70px',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+            }}
           >
-            <Stack gap='md' style={{ padding: '20px' }}>
-              <Title>Your Health Ally</Title>
-              <Text>Ask your questions here.</Text>
+            <Stack className='chatLog' gap='md' style={{ padding: '20px' }}>
+              <Group
+                className='chatUser'
+                bg={theme.colors.teal[2]}
+                style={{
+                  padding: '10px',
+                }}
+              >
+                <Avatar>AH</Avatar>
+                <Text>Ask your questions here.</Text>
+              </Group>
+              <Group
+                className='chatGPT'
+                bg={theme.colors.teal[5]}
+                style={{
+                  padding: '10px',
+                }}
+              >
+                <Avatar>GPT</Avatar>
+                <Text>I am an AI</Text>
+              </Group>
             </Stack>
 
             <Group
@@ -71,12 +92,13 @@ export function Chat() {
                 style={{
                   flex: 1,
                   marginRight: '20px',
-                  borderRadius: 10,
+                  borderRadius: '20px',
                   boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.1)',
                 }}
               />
               <Button
                 variant='filled'
+                color={theme.colors.pink[6]}
                 style={{
                   borderRadius: 10,
                   padding: '10px 20px',
@@ -87,17 +109,15 @@ export function Chat() {
               </Button>
             </Group>
           </AppShell.Main>
-
           <AppShell.Footer
             bg={theme.colors.teal[7]}
-            style={{ textAlign: 'center', padding: '20px' }}
+            style={{
+              textAlign: 'center',
+              padding: '20px',
+              marginTop: 'auto', // Pushes the footer to the bottom of the container
+            }}
           >
-            <Text
-              style={{
-                fontSize: '0.8rem',
-                textAlign: 'center',
-              }}
-            >
+            <Text style={{ fontSize: '0.8rem', textAlign: 'right' }}>
               © Your Health Ally. This is not medical advice. This is for
               educational purposes only. Please see your healthcare provider for
               medical treatment.
