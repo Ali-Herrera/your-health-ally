3e337d8d44e302a929305ab51b0214bc3fca8693
Victoria <vmcnorrill@gmail.com>
2024-03-18 16:16:52 -0400

    fixing errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,60 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { UserButton, useUser } from '@clerk/nextjs';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { httpBatchLink } from '@trpc/client';
-import { Group } from '@mantine/core';
+import { UserButton, useUser } from "@clerk/nextjs";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { Group } from "@mantine/core";
 
-import { Welcome } from '../components/Welcome/Welcome';
-import { Chat } from './chat';
-import { trpc } from '@/utils/trpc';
+import { Welcome } from "../components/Welcome/Welcome";
+import { Chat } from "./Chat/index";
+import { trpc } from "../utils/trpc";
+import { transformer } from "../utils/transformer";
 
 export default function HomePage() {
-  const { isLoaded, user } = useUser();
-  const [queryClient] = useState(() => new QueryClient());
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [
-        httpBatchLink({
-          url: 'http://localhost:3000/trpc',
-          // You can pass any HTTP headers you wish here
-          async headers() {
-            return {
-              authorization: getAuthCookie(),
-            };
-          },
-        }),
-      ],
-    }),
-  );
+	const { isLoaded, user } = useUser();
+	const [queryClient] = useState(() => new QueryClient());
+	const [trpcClient] = useState(() =>
+		trpc.createClient({
+			links: [
+				httpBatchLink({
+					// TODO: replace URL
+					url: "http://localhost:3000/trpc",
+					// You can pass any HTTP headers you wish here
+					async headers() {
+						return {
+							authorization: getAuthCookie(),
+						};
+					},
+					transformer: transformer,
+				}),
+			],
+		})
+	);
 
-  return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>
-      {/* If the user is not signed in, show the welcome page */}
-      {isLoaded && !user && (
-        <>
-          <Welcome />
-          <Group className='h-screen'>
-            <UserButton afterSignOutUrl='/' />
-          </Group>
-        </>
-      )}{' '}
-      {/* If the user is not signed in, show the welcome page */}
-      {isLoaded && user && (
-        <>
-          <Chat />
-          <Group className='h-screen'>
-            <UserButton afterSignOutUrl='/' />
-          </Group>
-        </>
-      )}
-      </QueryClientProvider>
-    </trpc.Provider>
-  );
+	return (
+		<trpc.Provider client={trpcClient} queryClient={queryClient}>
+			<QueryClientProvider client={queryClient}>
+				{/* If the user is not signed in, show the welcome page */}
+				{isLoaded && !user && (
+					<>
+						<Welcome />
+						<Group className="h-screen">
+							<UserButton afterSignOutUrl="/" />
+						</Group>
+					</>
+				)}{" "}
+				{/* If the user is not signed in, show the welcome page */}
+				{isLoaded && user && (
+					<>
+						<Chat />
+						<Group className="h-screen">
+							<UserButton afterSignOutUrl="/" />
+						</Group>
+					</>
+				)}
+			</QueryClientProvider>
+		</trpc.Provider>
+	);
 }
